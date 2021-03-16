1dc395a2d9e5fa2f0b3077f3762fac7e688e6232
Lukasj17 <79445231+Lukasj17@users.noreply.github.com>
2021-03-16 13:17:25 +0200

    sandauga

---
+++ js/13-multiply.js
@@ -0,0 +1,10 @@
+function daugyba(a, b) {
+    const rez = a * b;
+    return rez;
+}
+
+console.log(daugyba(4, 5));
+console.log(daugyba(224, 5));
+console.log(daugyba(-1234, 5));
+console.log(daugyba(4.14, 5));
+console.log(daugyba(-3.14, 2));
