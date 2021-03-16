eb99a111ad47264dc5d14b988851e402ef4e9cc0
Lukasj17 <79445231+Lukasj17@users.noreply.github.com>
2021-03-16 13:00:33 +0200

    functions

---
+++ js/11-suma.js
@@ -0,0 +1,14 @@
+const a = 8;
+const b = 5;
+const c = 834;
+const d = 543;
+const e = 832;
+const f = 5111;
+
+function desimteriopaSuma (pirmas, antras) {
+    const s = (pirmas + antras) * 10;
+    console.log(s);
+}
+
+desimteriopaSuma(a, d);
+desimteriopaSuma(c, c);
